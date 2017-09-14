c80b4a28ed9c059e946aca66e3da3f8c219e62ab
HrebtovAlexey <31938743+HrebtovAlexey@users.noreply.github.com>
2017-09-14 23:22:12 +0300

    Add files via upload

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.MaxValue = null;
+    	this.MinValue = null;
+    	this.middleValue = null;
+    }
 
     setRange(min, max) {
-
+    	this.maxValue = max;
+    	this.minValue = min;
     }
 
     guess() {
-
+    	this.middleValue = Math.ceil((this.minValue + this.maxValue)/2);
+    	return this.middleValue;
     }
 
     lower() {
-
+    	this.maxValue = this.middleValue;
+    	this.middleValue = Math.ceil((this.minValue + this.maxValue)/2);
+    	return this.middleValue;
     }
 
     greater() {
-
+		this.minValue = this.middleValue;
+    	this.middleValue = Math.ceil((this.minValue + this.maxValue)/2);
+    	return this.middleValue;
     }
 }
 
